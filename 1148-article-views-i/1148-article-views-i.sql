af1e1d99b5d1f6e6fa6eedd627b2b810fa6c70d1
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-05-25 23:54:48 +0200

    Time: 560 ms (19.22%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -2,5 +2,5 @@
 
 Select Distinct V1.author_id as id
 from Views as V1, Views as V2
-where v1.author_id = v2.viewer_id AND v1.author_id = v2.author_id 
+where  v1.author_id = v2.author_id  AND v1.author_id = v2.viewer_id
 order by v1.author_id
