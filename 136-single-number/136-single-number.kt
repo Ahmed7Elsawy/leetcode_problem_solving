9f57d5bdf2fdb130e50b654f538913d21c800b4f
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-06-01 21:48:42 +0200

    Time: 365 ms (66.99%), Space: 49.9 MB (40.78%) - LeetHub

--- 136-single-number/136-single-number.kt
+++ 136-single-number/136-single-number.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun singleNumber(nums: IntArray): Int {
-        val sortedArray = nums.sortedBy { it }
-        return sortedArray.filterIndexed { index, value -> 
-            !(index > 0 && value == sortedArray[index - 1] ||
-                index < sortedArray.lastIndex && value == sortedArray[index + 1]) }
-        .first()
+        var result = 0
+        for (num in nums)
+            result = result xor num
+
+        return result
     }
 }
