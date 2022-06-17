db71c08a7cb97ba9569895df1f9258e5cc00cb69
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-06-17 17:13:26 +0200

    Time: 443 ms (5.04%), Space: 42.7 MB (18.31%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.kt
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.kt
@@ -20,10 +20,9 @@ class Solution {
             return index to root
         }
 
-        val index1 = if (index == 0) 0 else index //+ 1
         var pair1: Pair<Int, TreeNode?>? = null
         if (root.left != null)
-            pair1 = kthSmallestNode(root.left, k, index1)
+            pair1 = kthSmallestNode(root.left, k, index)
 
         if (pair1?.first == k) {
             return pair1
@@ -31,7 +30,7 @@ class Solution {
 
         val index2 = when (pair1?.first) {
             null -> index + 1
-            index1 -> pair1.first
+            index -> pair1.first
             else -> pair1.first + 1
         }
 
@@ -39,11 +38,8 @@ class Solution {
             return index2 to root
         }
 
-//        println("${index2}  -  ${root?.`val`}  --")
-
         val nod2 = kthSmallestNode(root.right, k, index2)
         return if (root.right != null) {
-//            println("${nod2.first}  -  ${nod2.second?.`val`}")
             nod2
         }else index2 to root
     }
