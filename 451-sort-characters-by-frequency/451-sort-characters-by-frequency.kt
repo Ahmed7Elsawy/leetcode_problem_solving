8d862167148e0b03b68a345c2b383c0b4adb6853
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-06-21 22:00:28 +0200

    Time: 353 ms (65.45%), Space: 49.8 MB (32.73%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.kt
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.kt
@@ -3,13 +3,13 @@ class Solution {
     fun frequencySort(s: String): String {
         val x = s.toCharArray().groupBy { it }
             .map { it.key to it.value.size }.sortedByDescending { it.second }
-        val arr = mutableListOf<Char>()
+        val arr = StringBuilder()
         for (i in x) {
             repeat(i.second) {
-                arr.add(i.first)
+                arr.append(i.first)
             }
         }
-        return arr.joinToString("")
+        return arr.toString()
     }
 
 }
