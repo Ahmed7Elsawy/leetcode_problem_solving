5891ad47f27e9ab6a2b1b2ebfa9294adbc0dde96
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-06-26 21:50:02 +0200

    Time: 217 ms (74.95%), Space: 35.4 MB (14.96%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.kt
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.kt
@@ -9,24 +9,20 @@
  */
 class Solution {
     private val list = mutableListOf<ListNode?>()
+    
     fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
         var current = head
         while (current != null) {
             list.add(current)
             current = current.next
         }
-        
+
         if (list.size - n == 0)
             return head?.next
-        
-        current = head
-        var beforeCurrent: ListNode? = null
-        for (i in 1..(list.size - n)) {
-            beforeCurrent = current
-            current = current?.next
-        }
-        beforeCurrent?.next = current?.next
+
+        list[list.size - n - 1]?.next = list[list.size - n]?.next
         return head
     }
+
     
 }
