28b9bb850c68929787c9b60d3e739dbfd538e392
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-06-29 13:41:25 +0200

    Time: 1 ms (89.89%), Space: 48.1 MB (48.10%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,32 +1,26 @@
 class Solution {
-    int[][] images;
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-
         int oldColor = image[sr][sc];
         if (oldColor == color)
             return image;
-
-        images = image;
-        dfs(sr, sc, oldColor, color);
-
-        return images;
+        dfs(image, sr, sc, oldColor, color);
+        return image;
     }
 
-    private void dfs(int sr, int sc, int oldColor, int color) {
-        if (images[sr][sc] != oldColor) return;
+    private void dfs(int[][] image, int sr, int sc, int oldColor, int color) {
+        if (image[sr][sc] != oldColor) return;
 
-        if (images[sr][sc] == oldColor)
-            images[sr][sc] = color;
+        if (image[sr][sc] == oldColor)
+            image[sr][sc] = color;
 
         if (sr - 1 >= 0)
-            dfs(sr - 1, sc, oldColor, color);
+            dfs(image,sr - 1, sc, oldColor, color);
         if (sc - 1 >= 0)
-            dfs(sr, sc - 1, oldColor, color);
+            dfs(image,sr, sc - 1, oldColor, color);
 
-        if (sr + 1 < images.length)
-            dfs(sr + 1, sc, oldColor, color);
-        if (sc + 1 < images[0].length)
-            dfs(sr, sc + 1, oldColor, color);
+        if (sr + 1 < image.length)
+            dfs(image,sr + 1, sc, oldColor, color);
+        if (sc + 1 < image[0].length)
+            dfs(image, sr, sc + 1, oldColor, color);
     }
 }
