317aa700b0ba00756245b5c19b5691bdfec1ecb1
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-02 23:27:00 +0200

    Time: 846 ms (20.39%), Space: 112.3 MB (13.59%) - LeetHub

---
+++ 77-combinations/77-combinations.kt
@@ -0,0 +1,18 @@
+class Solution {
+    private val list = mutableListOf<List<Int>>()
+    fun combine(n: Int, k: Int): List<List<Int>> {
+        combine(n, k, 1, emptyList())
+        return list
+    }
+
+    private fun combine(n: Int, k: Int, current: Int, list2: List<Int>) {
+        if (list2.size == k) {
+            list.add(list2)
+            return
+        }
+        if (list2.size > k || current > n) return
+
+        combine(n, k, current + 1, list2.toMutableList().apply { add(current) })
+        combine(n, k, current + 1, list2)
+    }
+}
