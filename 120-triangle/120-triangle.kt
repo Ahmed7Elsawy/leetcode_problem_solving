18b86978d64bda6c6c496537e2ccba4ebbb9adb9
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-04 00:31:02 +0200

    Time: 313 ms (58.66%), Space: 48.3 MB (9.89%) - LeetHub

---
+++ 120-triangle/120-triangle.kt
@@ -0,0 +1,25 @@
+import kotlin.math.min
+
+class Solution {
+
+    private lateinit var myTriangle: List<MutableList<Int>>
+    fun minimumTotal(triangle: List<List<Int>>): Int {
+        myTriangle =
+            List<MutableList<Int>>(triangle.size + 1) { MutableList<Int>(triangle[triangle.lastIndex].size) { -1 } }
+
+        return calculate(triangle, 0, 0)
+    }
+
+    private fun calculate(triangle: List<List<Int>>, i: Int, j: Int): Int {
+        if (i >= triangle.size) return 0
+        if (j >= triangle[i].size) return 100000
+
+        if (myTriangle[i][j] != -1) return myTriangle[i][j]
+
+        val path2 = calculate(triangle, i + 1, j)
+        val path3 = calculate(triangle, i + 1, j + 1)
+        myTriangle[i][j] = triangle[i][j] + min(path2, path3)
+        return myTriangle[i][j]
+    }
+
+}
