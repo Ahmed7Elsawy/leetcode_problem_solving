daa3daca847c84c823c7d0be42cc3b5f49942dbe
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-08 02:51:29 +0200

    Time: 370 ms (76.97%), Space: 51.3 MB (11.51%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.kt
+++ 1929-concatenation-of-array/1929-concatenation-of-array.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun getConcatenation(nums: IntArray): IntArray {
-        val list = nums.toMutableList()
-        list.addAll(nums.toMutableList())
-        return list.toIntArray()
+        return nums + nums
     }
 }
