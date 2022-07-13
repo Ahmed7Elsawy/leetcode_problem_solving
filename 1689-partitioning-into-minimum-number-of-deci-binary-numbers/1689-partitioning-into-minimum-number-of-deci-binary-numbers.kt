ddde824e3b250de9b4ca13703679f0c343026803
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-13 04:52:12 +0200

    Time: 371 ms (60.80%), Space: 66.7 MB (28.90%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.kt
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun minPartitions(n: String): Int {
-        var count = 0
-        var num1 = n.map { it }
-        while (num1.isNotEmpty()) {
-            count++
-            num1 = num1.mapNotNull { if (it == '0' || it - 1 == '0') null else it - 1 }
-        }
-        return count
-    }
+    fun minPartitions(n: String) = n?.maxBy { it - '0' }?.minus('0') ?: 0
 }
