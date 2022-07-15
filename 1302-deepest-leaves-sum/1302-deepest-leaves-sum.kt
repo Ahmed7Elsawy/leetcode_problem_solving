f2ed45befda38835d06bcf4a45e78d402aad0ebf
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-15 16:26:48 +0200

    Time: 726 ms (13.01%), Space: 70.9 MB (41.91%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.kt
@@ -0,0 +1,28 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    private val arr = mutableListOf<Int>()
+    fun deepestLeavesSum(root: TreeNode?): Int {
+        if (root == null) return 0
+        sum(root, 0)
+        return arr.last()
+    }
+
+    private fun sum(root: TreeNode, level: Int) {
+        if (level >= arr.size) {
+            arr.add(root.`val`)
+        } else {
+            arr[level] = arr[level] + root.`val`
+        }
+        root.left?.let { sum(it, level + 1) }
+        root.right?.let { sum(it, level + 1) }
+    }
+}
