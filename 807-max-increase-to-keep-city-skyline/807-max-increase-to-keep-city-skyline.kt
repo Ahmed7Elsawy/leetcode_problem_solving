be7a385c58d769f184dbf7993905eaf0ccb0f54b
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-16 21:26:55 +0200

    Time: 407 ms (9.09%), Space: 44.5 MB (22.73%) - LeetHub

--- 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.kt
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.kt
@@ -1,19 +1,21 @@
 import kotlin.math.min
 
 class Solution {
-    private val max = mutableListOf<MutableList<Int>>()
+    private val maxColumns = mutableListOf<Int>()
     fun maxIncreaseKeepingSkyline(grid: Array<IntArray>): Int {
         var sum = 0
         for (i in 0..grid.lastIndex) {
-            max.add(mutableListOf())
+            val maxRow = grid[i].max() ?: 0
             for (j in 0..grid[0].lastIndex) {
-                var maxColumn = -1
-                for (x in 0..grid.lastIndex) {
-                    if (grid[x][j] > maxColumn)
-                        maxColumn = grid[x][j]
+                if (i == 0) {
+                    var maxColumn = -1
+                    for (x in 0..grid.lastIndex) {
+                        if (grid[x][j] > maxColumn)
+                            maxColumn = grid[x][j]
+                    }
+                    maxColumns.add(maxColumn)
                 }
-                max[i].add(min(grid[i].max() ?: 0, maxColumn))
-                sum += (max[i][j] - grid[i][j])
+                sum += (min(maxRow, maxColumns[j]) - grid[i][j])
             }
         }
         return sum
