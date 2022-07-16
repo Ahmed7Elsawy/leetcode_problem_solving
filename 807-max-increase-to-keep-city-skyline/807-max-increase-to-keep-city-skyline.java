72b2ec3d0ed7c95b427143b3bc6e2bef9843c636
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-16 21:47:07 +0200

    Time: 6 ms (7.70%), Space: 44.7 MB (15.94%) - LeetHub

---
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    private ArrayList<Integer> maxColumns = new ArrayList<>();
+
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            int maxRow = Arrays.stream(grid[i]).max().getAsInt();
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0) {
+                    int max = -1;
+                    for (int[] ints : grid) {
+                        if (max < ints[j]) {
+                            max = ints[j];
+                        }
+                    }
+                    maxColumns.add(max);
+                }
+                sum += (Math.min(maxRow, maxColumns.get(j)) - grid[i][j]);
+            }
+        }
+        return sum;
+    }
+}
