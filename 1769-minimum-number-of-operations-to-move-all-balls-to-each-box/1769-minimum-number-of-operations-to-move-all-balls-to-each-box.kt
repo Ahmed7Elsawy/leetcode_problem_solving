3ff93f9692ab1437661fb4815138be20fe03b936
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-17 22:11:17 +0200

    Time: 538 ms (57.69%), Space: 46.1 MB (71.15%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.kt
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.kt
@@ -4,12 +4,10 @@ class Solution {
     private lateinit var result: IntArray
     fun minOperations(boxes: String): IntArray {
         result = IntArray(boxes.length) { 0 }
-
         for (i in boxes.indices) {
+            if (boxes[i] == '0') continue
             for (j in boxes.indices) {
-                if (boxes[j] == '1') {
-                    result[i] += abs(i - j)
-                }
+                result[j] += abs(i - j)
             }
         }
         return result
