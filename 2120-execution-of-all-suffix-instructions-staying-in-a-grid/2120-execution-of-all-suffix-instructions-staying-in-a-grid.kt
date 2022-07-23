0cc4b92c3f346b43c62731521f26328a158bf0bb
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-23 21:10:10 +0200

    Time: 577 ms (50.00%), Space: 104.1 MB (25.00%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.kt
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.kt
@@ -2,29 +2,29 @@ class Solution {
     private val list = mutableListOf<Int>()
     fun executeInstructions(n: Int, startPos: IntArray, s: String): IntArray {
         for (i in s.indices) {
-            dfs(n, startPos, s.substring(i), Pair(startPos[0], startPos[1]), 0)
+            dfs(n, startPos, s, Pair(startPos[0], startPos[1]), 0, i)
         }
         return list.toIntArray()
     }
 
-    private fun dfs(n: Int, startPos: IntArray, s: String, currentPos: Pair<Int, Int>, count: Int) {
+    private fun dfs(n: Int, startPos: IntArray, s: String, currentPos: Pair<Int, Int>, count: Int, index: Int) {
         if (currentPos.first < 0 || currentPos.first >= n || currentPos.second < 0 || currentPos.second >= n) {
             list.add(count - 1)
             return
         }
-        if (s.isEmpty()) {
+        if (s.length == index) {
             list.add(count)
             return
         }
-        when (s[0]) {
-            'U' -> dfs(n, startPos, s.substring(1),
-                       Pair(currentPos.first - 1, currentPos.second), count + 1)
-            'D' -> dfs(n, startPos, s.substring(1),
-                       Pair(currentPos.first + 1, currentPos.second), count + 1)
-            'L' -> dfs(n, startPos, s.substring(1),
-                       Pair(currentPos.first, currentPos.second - 1), count + 1)
-            'R' -> dfs(n, startPos, s.substring(1),
-                       Pair(currentPos.first, currentPos.second + 1), count + 1)
+        when (s[index]) {
+            'U' -> dfs(n, startPos, s,
+                       Pair(currentPos.first - 1, currentPos.second),count + 1, index + 1)
+            'D' -> dfs(n, startPos, s,
+                       Pair(currentPos.first + 1, currentPos.second), count + 1, index + 1)
+            'L' -> dfs(n, startPos, s,
+                       Pair(currentPos.first, currentPos.second - 1), count + 1, index + 1)
+            'R' -> dfs(n, startPos, s,
+                       Pair(currentPos.first, currentPos.second + 1), count + 1, index + 1)
         }
     }
 }
