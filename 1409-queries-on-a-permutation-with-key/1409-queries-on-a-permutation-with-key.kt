bc9b1ffd4b46aa3ba1fa837e706e365f9718d352
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-25 23:16:59 +0200

    Time: 428 ms (50.00%), Space: 41.6 MB (50.00%) - LeetHub

--- 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.kt
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.kt
@@ -1,12 +1,16 @@
+
 class Solution {
     fun processQueries(queries: IntArray, m: Int): IntArray {
-        val list = MutableList(m) { it + 1 }
+        val list2 = Array(m) { it + 1 }
+        println(list2.joinToString())
         val result = mutableListOf<Int>()
         for (i in queries) {
-            val index = list.indexOf(i)
+            val index = list2.indexOf(i)
             result.add(index)
-            list.removeAt(index)
-            list.add(0, i)
+            for (index in index downTo 1) {
+                list2[index] = list2[index - 1]
+            }
+            list2[0] = i
         }
         return result.toIntArray()
     }
