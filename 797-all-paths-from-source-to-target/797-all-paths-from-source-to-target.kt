81a66d16d00c6617eb882f625a77ecb68ca41f1a
Ahmed Elsawy <32717670+Ahmed7Elsawy@users.noreply.github.com>
2022-07-28 18:18:23 +0200

    Time: 744 ms (9.79%), Space: 73.5 MB (5.59%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.kt
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.kt
@@ -1,19 +1,15 @@
 class Solution {
-    private lateinit var graph1: Array<IntArray>
     private val list = mutableListOf<List<Int>>()
     fun allPathsSourceTarget(graph: Array<IntArray>): List<List<Int>> {
-        graph1 = graph
-        bfs(0, listOf(0))
+        bfs(0, listOf(0), graph)
         return list
     }
-
-    private fun bfs(index: Int, currentList: List<Int>) {
-        if (index == graph1.lastIndex) {
-            list.add(currentList)
-        }
+    private fun bfs(index: Int, currentList: List<Int>, graph1: Array<IntArray>) {
         for (i in graph1[index]) {
-            bfs(i, currentList.toMutableList().apply { add(i) })
+            if (i == graph1.lastIndex) {
+                list.add(currentList.toMutableList().apply { add(i) })
+            } else
+                bfs(i, currentList.toMutableList().apply { add(i) }, graph1)
         }
     }
-
 }
